d744036eedc416e839afac02abe9d4853ccff5d6
Jin Jin <bmouse1006@msn.com>
2013-01-24 11:09:04 +0800

    实现视频拖动播放

---
+++ iShare/ThirdParty/JJMediaPlayerController/NSString+movieplayer.m
@@ -0,0 +1,13 @@
+//
+//  NSString+movieplayer.m
+//  iShare
+//
+//  Created by Jin Jin on 13-1-23.
+//  Copyright (c) 2013年 Jin Jin. All rights reserved.
+//
+
+#import "NSString+movieplayer.h"
+
+@implementation NSString (movieplayer)
+
+@end
